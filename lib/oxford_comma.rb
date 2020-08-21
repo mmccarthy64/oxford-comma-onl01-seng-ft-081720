2c54e979c0351bccfc952561102f98a20ba03b45
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-08-21 17:47:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif
+    array.size == 2
+    array.join(" and ")
+  else
+    array.each.collect do |name|
+      if name == array[-1]
+        array[-1] = "and " << name
+      end
+    end
+    array.join(", ")
+  end
 end
